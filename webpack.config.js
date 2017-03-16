ea0aac19e049584a72a9a1f8dee0111563f26bc9
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2017-03-16 05:24:50 -0400

    step 1

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+}
